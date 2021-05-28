192a75287ae08b6496671ffc7836c69a672f59d1
devansh2001 <dponda3@gatech.edu>
2021-05-28 00:17:38 -0400

    added another button for demo

--- src/ReactSSExample.js
+++ src/ReactSSExample.js
@@ -6,9 +6,10 @@ const Example = () => {
   const { image, takeScreenshot } = useScreenshot({ref: document.getElementById('some-id')});
   return (
     <div>
-        <div id='some-id'>
+        <div id='some-id' style={{border: '1px solid red'}}>
             <h1>Hello World!</h1>
-            <button>Some button</button>
+            <button>Button</button>
+            <p>This is some text content</p>
         </div>
         <button onClick={() => takeScreenshot()}>Take Screenshot</button>
         {image && <img src={image} />}
